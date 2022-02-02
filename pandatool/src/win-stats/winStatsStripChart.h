2a5bb0338ae4e28dc6f42766714c7cb8fd408ff8
rdb <git@rdb.name>
2022-02-02 19:17:02 +0100

    pstatserver: Major functional and aesthetic improvements to  PStats, including:
    * New "Flame Graph" chart for seeing all collectors in a frame, much easier to read than piano roll
    * Update controls, fonts, background color to more modern visual style on Windows
    * Proper handling of high DPI monitors
    * Add tooltips for collector labels showing full name and averaged value
    * Colors of collectors are now converted to sRGB transfer encoding
    * Major performance improvement to piano roll view on Windows
    * Movering mouse over labels now highlights the corresponding area in chart
    * Label hover effect changed to darkening effect instead of border
    * Reimplement graph as static common control on Windows
    * Check boxes are now clickable by their label on Windows
    * Graph windows have minimum sizes on Windows

--- pandatool/src/win-stats/winStatsStripChart.h
+++ pandatool/src/win-stats/winStatsStripChart.h
@@ -43,7 +43,7 @@ public:
 
   virtual void set_time_units(int unit_mask);
   virtual void set_scroll_speed(double scroll_speed);
-  virtual void clicked_label(int collector_index);
+  virtual void on_click_label(int collector_index);
   void set_vertical_scale(double value_height);
 
 protected:
@@ -79,7 +79,6 @@ private:
   std::string _net_value_text;
 
   HWND _smooth_check_box;
-  static size_t _check_box_height, _check_box_width;
 
   static bool _window_class_registered;
   static const char * const _window_class_name;
